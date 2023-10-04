c3376f7b4879ffc1e554acbe118a94b930ecffec
Carlos <carlosdanieltc@gmail.com>
2023-10-04 13:32:40 +0200

    fix(*): Changes for deployment

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,39 @@
+// import { nodeResolve } from "@rollup/plugin-node-resolve";
+// import { terser } from "rollup-plugin-terser";
+// import { babel } from "@rollup/plugin-babel";
+// import copy from "rollup-plugin-copy";
+
+// export default {
+//   input: "index.js",       
+//   output: {
+//     file: "docs/bundle.js",
+//     format: "esm",
+//   },
+//   plugins: [
+//     nodeResolve(),
+//     babel({ babelHelpers: "bundled" }),
+//     terser(),
+//     copy({
+//       targets: [
+//         { src: "assets/*", dest: "docs/assets" }, // Copia la carpeta "assets" a "dist/assets"
+//         { src: "index.html", dest: "docs" }, // Copia index.html a la carpeta "dist"
+//         { src: "manifest.json", dest: "docs" }, 
+//         { src: "indexeddb.js", dest: "docs" }, 
+//       ],
+//     }),
+//   ],
+//   watch: {
+//     clearScreen: false,
+//   },
+// };
+
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import { babel } from "@rollup/plugin-babel";
 import copy from "rollup-plugin-copy";
 
 export default {
-  input: "index.js",       
+  input: "index.js",
   output: {
     file: "docs/bundle.js",
     format: "esm",
@@ -14,12 +43,7 @@ export default {
     babel({ babelHelpers: "bundled" }),
     terser(),
     copy({
-      targets: [
-        { src: "assets/*", dest: "docs/assets" }, // Copia la carpeta "assets" a "dist/assets"
-        { src: "index.html", dest: "docs" }, // Copia index.html a la carpeta "dist"
-        { src: "manifest.json", dest: "docs" }, 
-        { src: "indexeddb.js", dest: "docs" }, 
-      ],
+      targets: [{ src: "public/*", dest: "dist" }], 
     }),
   ],
   watch: {
